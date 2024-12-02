c54c057d57ec9c5299642d94e86e0b59449b51c7
GopStop-dev <ritterecom@gmail.com>
2024-12-02 11:04:33 +0500

    my commit

---
+++ if.py
@@ -0,0 +1,11 @@
+num1 = int(input("Birinchi sonni kiriting: "))
+num2 = int(input("Ikkinchi sonni kiriting: "))
+
+if num1 == num2:
+    num1 = num1 + num2
+    num2 = num1 + num2
+    print(num1)
+else:
+    num1 = 0
+    num2 = 0
+    print(num2)
